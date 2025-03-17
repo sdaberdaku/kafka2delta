9623176c843ef852203c415ceeccc44f07515a6e
sdaberdaku <sebastian.daberdaku@cardoai.com>
2025-03-17 11:48:46 +0100

    refactor avro schema for improved readability and updated namespace

--- src/kafka2delta/utils/test/avro_schema.py
+++ src/kafka2delta/utils/test/avro_schema.py
@@ -1,4 +1,35 @@
-avro_schema_str = "{\"type\":\"record\",\"name\":\"Value\",\"namespace\":\"apollo.public.santiagocapital_assetlog\",\"fields\":[{\"name\":\"id\",\"type\":{\"type\":\"int\",\"connect.default\":0},\"default\":0},{\"name\":\"created_at\",\"type\":[\"null\",{\"type\":\"string\",\"connect.version\":1,\"connect.name\":\"io.debezium.time.ZonedTimestamp\"}],\"default\":null},{\"name\":\"updated_at\",\"type\":[\"null\",{\"type\":\"string\",\"connect.version\":1,\"connect.name\":\"io.debezium.time.ZonedTimestamp\"}],\"default\":null},{\"name\":\"date\",\"type\":{\"type\":\"int\",\"connect.version\":1,\"connect.name\":\"org.apache.kafka.connect.data.Date\",\"logicalType\":\"date\"}},{\"name\":\"loan_connection_name\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"property_address\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"town\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"county\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"postcode\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"loan_type\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"currency_abbreviation\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"gross_loan_amount\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"fasanara_allocation\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"repayment_type\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"repayment_schedule\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"security_charge\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"drawdown_date\",\"type\":[\"null\",{\"type\":\"int\",\"connect.version\":1,\"connect.name\":\"org.apache.kafka.connect.data.Date\",\"logicalType\":\"date\"}],\"default\":null},{\"name\":\"redemption_date\",\"type\":[\"null\",{\"type\":\"int\",\"connect.version\":1,\"connect.name\":\"org.apache.kafka.connect.data.Date\",\"logicalType\":\"date\"}],\"default\":null},{\"name\":\"asset_id\",\"type\":[\"null\",\"int\"],\"default\":null},{\"name\":\"__deleted\",\"type\":[\"null\",\"string\"],\"default\":null},{\"name\":\"__timestamp\",\"type\":[\"null\",\"long\"],\"default\":null},{\"name\":\"__log_sequence_number\",\"type\":[\"null\",\"long\"],\"default\":null}],\"connect.name\":\"apollo.public.santiagocapital_assetlog.Value\"}"
+avro_schema_str = """
+{
+    "type": "record",
+    "name": "Value",
+    "namespace": "tenant.public.test_assetlog",
+    "fields": [
+        {"name": "id", "type": {"type": "int", "connect.default": 0}, "default": 0},
+        {"name": "created_at", "type": ["null", {"type": "string", "connect.version": 1, "connect.name": "io.debezium.time.ZonedTimestamp"}], "default": null},
+        {"name": "updated_at", "type": ["null", {"type": "string", "connect.version": 1, "connect.name": "io.debezium.time.ZonedTimestamp"}], "default": null},
+        {"name": "date", "type": {"type": "int", "connect.version": 1, "connect.name": "org.apache.kafka.connect.data.Date", "logicalType": "date"}},
+        {"name": "loan_connection_name", "type": ["null", "string"], "default": null},
+        {"name": "property_address", "type": ["null", "string"], "default": null},
+        {"name": "town", "type": ["null", "string"], "default": null},
+        {"name": "county", "type": ["null", "string"], "default": null},
+        {"name": "postcode", "type": ["null", "string"], "default": null},
+        {"name": "loan_type", "type": ["null", "string"], "default": null},
+        {"name": "currency_abbreviation", "type": ["null", "string"], "default": null},
+        {"name": "gross_loan_amount", "type": ["null", "string"], "default": null},
+        {"name": "allocation", "type": ["null", "string"], "default": null},
+        {"name": "repayment_type", "type": ["null", "string"], "default": null},
+        {"name": "repayment_schedule", "type": ["null", "string"], "default": null},
+        {"name": "security_charge", "type": ["null", "string"], "default": null},
+        {"name": "drawdown_date", "type": ["null", {"type": "int", "connect.version": 1, "connect.name": "org.apache.kafka.connect.data.Date", "logicalType": "date"}], "default": null},
+        {"name": "redemption_date", "type": ["null", {"type": "int", "connect.version": 1, "connect.name": "org.apache.kafka.connect.data.Date", "logicalType": "date"}], "default": null},
+        {"name": "asset_id", "type": ["null", "int"], "default": null},
+        {"name": "__deleted", "type": ["null", "string"], "default": null},
+        {"name": "__timestamp", "type": ["null", "long"], "default": null},
+        {"name": "__log_sequence_number", "type": ["null", "long"], "default": null}
+    ],
+    "connect.name": "tenant.public.test_assetlog.Value"
+}
+"""
 
 b64_encoded_values = [
     "AAAABhdAAjYyMDI0LTA3LTExVDA3OjEzOjMwLjQ1NjE0MVoCNjIwMjQtMDctMTFUMDc6MTM6MzAuNDU2MTUzWpa3AgAAAAAAAAAAAhAwLjAwMDAwMAAAAAACircCAhQCCmZhbHNlApyY9Z6mZAKAgOb+gZEe",
