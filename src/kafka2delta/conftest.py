abe7daccae519824f509da3b21d483e58e1d52a5
Yauheni Subota <zhenya.subbota.19@gmail.com>
2026-09-07 18:18:24 +0200

    chore(python-313-deps): 3.13 deps

--- src/kafka2delta/conftest.py
+++ src/kafka2delta/conftest.py
@@ -87,9 +87,9 @@ def spark(s3_bucket: str) -> Generator[SparkSession, None, None]:
         "com.amazonaws:aws-java-sdk-bundle:1.12.262",
         "io.delta:delta-spark_2.12:3.2.0",
         "org.apache.hadoop:hadoop-aws:3.3.4",
-        "org.apache.spark:spark-avro_2.12:3.5.1",
-        "org.apache.spark:spark-streaming-kafka-0-10_2.12:3.5.1",
-        "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1"
+        "org.apache.spark:spark-avro_2.12:3.5.5",
+        "org.apache.spark:spark-streaming-kafka-0-10_2.12:3.5.5",
+        "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.5"
     }
     with tempfile.TemporaryDirectory() as tempdir:
         spark = (
